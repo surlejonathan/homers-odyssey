4d6cf12adaacb183a23fbb9cd9b534ccd15b1fb2
surlejonathan <surle.jonathan@gmail.com>
2020-12-22 20:15:59 +0100

    quete 020

--- front/app/src/components/SignUp.js
+++ front/app/src/components/SignUp.js
@@ -4,23 +4,70 @@ class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "test@test.fr",
+      email: "",
+      password: "",
+      passwordBis: "",
+      name: "",
+      lastname: "",
     };
-    this.udpateEmailField = this.udpateEmailField.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  udpateEmailField(e) {
-    const { email } = this.state;
-    this.setState({ email: e.target.value });
-  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({
+      email: "",
+      password: "",
+      passwordBis: "",
+      name: "",
+      lastname: "",
+    });
+  };
+
   render() {
+    const { email, password, passwordBis, name, lastname } = this.state;
     return (
       <div>
-        <h1>{this.state.email}</h1>
-        <input
-          type='email'
-          name='email'
-          onChange={(e) => this.udpateEmailField(e)}
-        />
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={this.handleChange}
+          />{" "}
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={this.handleChange}
+          />{" "}
+          <input
+            type='password'
+            name='passwordBis'
+            value={passwordBis}
+            onChange={this.handleChange}
+          />{" "}
+          <input
+            type='name'
+            name='name'
+            value={name}
+            onChange={this.handleChange}
+          />{" "}
+          <input
+            type='lastname'
+            name='lastname'
+            value={lastname}
+            onChange={this.handleChange}
+          />{" "}
+          <input type='submit' value='Submit' />
+        </form>
       </div>
     );
   }
